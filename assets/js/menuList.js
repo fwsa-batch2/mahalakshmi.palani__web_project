63407699fe928df2f581abc5d7c960aeea5cccd6
mahalakshmi2000 <mahalakshmiragaviragavi@gmail.com>
2021-12-27 20:02:28 +0530

    added js code

--- assets/js/menuList.js
+++ assets/js/menuList.js
@@ -4,41 +4,48 @@ function onPageLoad() {
 
   if (listValue != null) {
     userlist = listValue;
-  } 
-  else {
+  } else {
     // listvalue = null;
     userlist = [];
     localStorage.setItem("foodlist", JSON.stringify(userlist));
   }
-  return userlist;
+  // return userlist;
 }
 onPageLoad();
 function renderFood(addItems){
-    let len = addItems.length;
-    console.log(len);
-    let add = "";
-    for(let i = 0; i < len; i++){
-        let obj = addItems[i];
-        let food = `<div id = "img-div2" class = "img-add">
+  let len = addItems.length;
+  console.log(len);
+  let adding;
+  for (let i = 0; i < len; i++) {
+    let obj = addItems[i];
+    let food = `<div id = "img-div2" class = "img-add">
                          <h2> ${obj.fname}</h2>
-                        <img src=${obj.img} class = "foodimg" alt="">
-                        <p> RS.${obj.price}</p><button type = "button" onclick = "clickHandler()">ADD CART</button>
+                        <img src=${obj.img} class = "foodimg" alt="foodname">
+                        <p> RS.${obj.price}</p><button type = "button" data-food onclick = "clickHandler()">ADD CART</button>
                     </div>`;
-        add +=food;
-    }
-    document.getElementById("addmenu").innerHTML = add;
-   
+    adding = adding + food;
+    // console.log(add);
+  }
+  document.getElementById("addmenu").innerHTML = adding;
 }
-
 
 let getValue = JSON.parse(localStorage.getItem("list"));
 renderFood(getValue);
 console.log(getValue);
+// function clickHandler(){
+//   let addfood = event.target.dataset.food;
+//   console.log(addfood);
+//   let objectfood = parsed.find()
+// }
 
- function clickHandler(value){
-   console.log(value);
- }
+//
+// foodArray = [];
+//  function clickHandler(){
+//   let foodDetails = JSON.parse(localStorage.getItem("list"));
+//   foodArray.push(foodDetails);
+//   console.log(foodArray);
+
+//  }
 // console.log("hello");
 // let getValue = JSON.parse(localStorage.getItem("list"));
 // console.log(getValue);
-
