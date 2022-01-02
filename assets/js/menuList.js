afed21516f32c3567de00b27174e4036c98c17ee
mahalakshmi2000 <mahalakshmiragaviragavi@gmail.com>
2022-01-02 16:57:11 +0530

    modified my file

--- assets/js/menuList.js
+++ assets/js/menuList.js
@@ -18,10 +18,10 @@ function renderFood(addItems){
   let adding;
   for (let i = 0; i < len; i++) {
     let obj = addItems[i];
-    let food = `<div id = "img-div2" class = "img-add">
+    let food = `<div id = "img-div${JSON.stringify(i+1)}" class = "img-add">
                          <h2> ${obj.fname}</h2>
                         <img src=${obj.img} class = "foodimg" alt="foodname">
-                        <p> RS.${obj.price}</p><button type = "button" data-food onclick = "clickHandler()">ADD CART</button>
+                        <p> RS.${obj.price}</p><button type = "button" data-food onclick = \"clickHandler('img-div${JSON.stringify(i+1)}')\">ADD CART</button>
                     </div>`;
     adding = adding + food;
     // console.log(add);
@@ -32,6 +32,13 @@ function renderFood(addItems){
 let getValue = JSON.parse(localStorage.getItem("list"));
 renderFood(getValue);
 console.log(getValue);
+function clickHandler(x){
+  let img = x; // img-div1,img-div2
+  console.log(img);
+  
+
+}
+// renderFood(getValue)
 // function clickHandler(){
 //   let addfood = event.target.dataset.food;
 //   console.log(addfood);
