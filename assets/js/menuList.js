bdc4a77c17d0a2891602440edc0fb2acee9d40e6
mahalakshmi2000 <mahalakshmiragaviragavi@gmail.com>
2022-01-06 14:27:09 +0530

    modified js code

--- assets/js/menuList.js
+++ assets/js/menuList.js
@@ -1,13 +1,14 @@
 let userlist = [];
+
 function onPageLoad() {
-  let listValue = JSON.parse(localStorage.getItem("foodlist")); // null
+  let listValue = JSON.parse(localStorage.getItem("fooditems")); // null
 
   if (listValue != null) {
     userlist = listValue;
   } else {
     // listvalue = null;
     userlist = [];
-    localStorage.setItem("foodlist", JSON.stringify(userlist));
+    localStorage.setItem("fooditems", JSON.stringify(userlist));
   }
   // return userlist;
 }
@@ -15,13 +16,13 @@ onPageLoad();
 function renderFood(addItems){
   let len = addItems.length;
   console.log(len);
-  let adding;
+  let adding ="";
   for (let i = 0; i < len; i++) {
     let obj = addItems[i];
     let food = `<div id = "img-div${JSON.stringify(i+1)}" class = "img-add">
-                         <h2> ${obj.fname}</h2>
+                        <h2> ${obj.fname}</h2>
                         <img src=${obj.img} class = "foodimg" alt="foodname">
-                        <p> RS.${obj.price}</p><button type = "button" data-food onclick = \"clickHandler('img-div${JSON.stringify(i+1)}')\">ADD CART</button>
+                        <p> RS.${obj.price}</p><button type = "button" data-food onclick = \"clickHandler('${obj.fname}','${obj.img}','${obj.price}')\">ADD CART</button>
                     </div>`;
     adding = adding + food;
     // console.log(add);
@@ -29,30 +30,25 @@ function renderFood(addItems){
   document.getElementById("addmenu").innerHTML = adding;
 }
 
-let getValue = JSON.parse(localStorage.getItem("list"));
+let getValue = JSON.parse(localStorage.getItem("fooditems"));
 renderFood(getValue);
 console.log(getValue);
-function clickHandler(x){
-  let img = x; // img-div1,img-div2
-  console.log(img);
+let imgArray = [];
+function clickHandler(name,url,price){
+  console.log(name);
+  console.log(url);
+  console.log(price);
+  let qty = prompt("enter the number of food quantity");
+  let cartobj = {
+    "imgname":name,
+    "imgurl":url,
+    "qty":qty,
+    "imgprice":price
+  }
+  imgArray.push(cartobj);
+  localStorage.setItem("cart",(JSON.stringify(imgArray)));
+  
   
 
 }
-// renderFood(getValue)
-// function clickHandler(){
-//   let addfood = event.target.dataset.food;
-//   console.log(addfood);
-//   let objectfood = parsed.find()
-// }
 
-//
-// foodArray = [];
-//  function clickHandler(){
-//   let foodDetails = JSON.parse(localStorage.getItem("list"));
-//   foodArray.push(foodDetails);
-//   console.log(foodArray);
-
-//  }
-// console.log("hello");
-// let getValue = JSON.parse(localStorage.getItem("list"));
-// console.log(getValue);
